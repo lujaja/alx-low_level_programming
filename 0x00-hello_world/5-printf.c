f1c470f2de53304eeb71e9e0b40995f2271b0f0b
lujaja <jarzcyber@gmail.com>
2023-03-16 14:39:37 -0700

    ALX S.E c programming practical

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,12 @@
 /**
  * main - entry point
  *
- * return: always (0) Success
+ * Return: always (0) Success
  */
-int main (void)
+int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n"
+ );
+	return(0);
 
 }
