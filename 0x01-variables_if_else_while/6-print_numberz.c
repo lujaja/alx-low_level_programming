edc8360c8499a0710ed781cbc2df747fd4c592d8
lujaja <jarzcyber@gmail.com>
2023-03-17 14:41:53 -0700

    ALX S.E c programming practical

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 always (success)
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 10)
+	{
+	putchar(a);
+	a++;
+	}
+putchar("\n");
+return (0);
+}
