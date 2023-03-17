649d285daeaba9d3f8b2b65ca597fb6131626998
lujaja <jarzcyber@gmail.com>
2023-03-17 13:14:47 -0700

    ALX S.E c programming practical

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: return 0 always (success)
+ */
+
+int main(void)
+{
+char alpha = 'a';
+while (alpha <= z)
+{
+	putchar(alpha);
+	alpha++;	
+}
+putchar('\n');
+return (0);
+}
+
