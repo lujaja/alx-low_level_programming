1a3d259e89875dca0cd25d19a226aae00711fbe5
lujaja <jarzcyber@gmail.com>
2023-03-17 14:29:27 -0700

    ALX S.E c programming practical

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,11 +8,13 @@
 
 int main(void)
 {
-	int i;
+	int a;
 
-for (i = 10; i < 10; i++)
-	printf("%i", i);
+	for (a = 0; a < 10; a++)
+	{
+		printf("%i", a);
+	}
+	putchar('\n');
 
-putchar('\n');
-return (0);
+	return (0);
 }
