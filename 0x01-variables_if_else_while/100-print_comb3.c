98128bbff2ff13ee5177f8c971724169dd862463
lujaja <jarzcyber@gmail.com>
2023-03-19 02:50:59 -0700

    ALX S.E c programming practical

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,8 +13,8 @@ int main(void)
 	{
 		for (num2= 0; num2 < 10; num2++)
 		{
-		putchar(num1 % 10 + '0';
-		putchar(num2 % 10 + '0';
+		putchar((num1 % 10) + '0';
+		putchar((num2 % 10) + '0';
 		if (num1 == 9 && num2 == 9;
 			continue;
 		putchar(',');
