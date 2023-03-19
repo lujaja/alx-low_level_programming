590ab253a0258bae28b7d100fa642a219d97de9b
lujaja <jarzcyber@gmail.com>
2023-03-19 01:52:03 -0700

    ALX S.E c programming practical

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - entry
- * Return: 0 (success)
+ * main - entry point
+ * Return: 0: alway success 
  */
-
 int main(void)
 {
 	int num;
-
 	for (num = 0; num <= 9; num++)
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-
 		putchar(',');
-		putchar(' ');
+		putchar('');
 	}
-
 	putchar('\n');
-
 	return (0);
 }
