27905eafbdd8106ede6a25a5d5cd74553ef19615
lujaja <jarzcyber@gmail.com>
2023-03-21 16:05:53 -0700

    ALX S.E c programming practical

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,19 @@
-#include <stdio.h>
+#include <main.h>
+/**
+ * main - entry
+ * Return: 0 always success
+ */
 
 int main(void)
 {
-	printf("_putchar\n");
+	char *lujaja = "_putchar";
+
+	while (*lujaja)
+	{
+		_putchar(*lujaja);
+		lujaja++;
+	}
+	_putchar('\n');
+
 	return (0);
 }
