5326df0d55114b9e6ad8ef77a5286f3331329cd9
lujaja <jarzcyber@gmail.com>
2023-03-21 17:58:28 -0700

    ALX S.E c programming practical

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lowercase.
+ *
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (aplha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+	_putchar('\n');
+
+	return (0);
+}
