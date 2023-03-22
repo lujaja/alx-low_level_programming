c52faefdf65c152b818aac56d4747be9db782cb7
lujaja <jarzcyber@gmail.com>
2023-03-21 19:32:32 -0700

    ALX S.E c programming practical

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * print_sign - checks whether a number is positve, negative or zero.
+  *
+  * @n: character.
+  *
+  * Return: 1 is its positve, 0 if n is zero and -1 if negative.
+  */
+
+int print_sign(int n)
+{
+	if (n > 1)
+	{
+		_putchar("+");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar("0");
+		return (0);
+	}
+	else
+	{
+		_putchar("-");
+		return (-1);
+	}
+}
