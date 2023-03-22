1d7cac11a3aeb716e723111ab5073e98a49e2c54
lujaja <jarzcyber@gmail.com>
2023-03-21 19:07:49 -0700

    ALX S.E c programming practical

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * _isalpha - checks if character is alpha.
+  * @c: character
+  * Return: 0 if not.
+  */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (C >= 'A' && C >= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
