acda0fd3408ab645e70e46e67a87bfd55dad47ed
lujaja <jarzcyber@gmail.com>
2023-03-21 18:15:43 -0700

    ALX S.E c programming practical

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times.
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int ten;
+
+	for (ten = 0; ten < 10; ten++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+	}
+}
+
