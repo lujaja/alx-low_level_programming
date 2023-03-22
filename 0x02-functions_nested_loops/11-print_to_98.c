102d74567a420e27d90c69074bd632752ee1bafc
lujaja <jarzcyber@gmail.com>
2023-03-21 23:42:25 -0700

    ALX S.E c programming practical

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print all number from n to 98
+ * @n: integer
+ */
+
+void print_to_98(int n)
+{
+	if (n < 99)
+	{
+		while (n < 99)
+		{
+			_putchar(n);
+			_putchar('\n');
+			n++;
+		}
+	}
+	else (n > 97)
+	{
+		while (n > 97)
+		{
+			_putchar(n;
+			_putchar('\n');
+			n--;
+		}
+	}
+}
