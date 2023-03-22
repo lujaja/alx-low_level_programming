5917b148c26a0dbddefdfc72747ae252ebeea76c
lujaja <jarzcyber@gmail.com>
2023-03-21 19:58:17 -0700

    ALX S.E c programming practical

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
-* _abs - absolute value
-* @t: the integer
-* return: value.
-*/
+ * _abs - absolute value
+ *
+ * @t: the integer
+ *
+ * Return: value.
+ */
 
 int _abs(int t)
 {
 	if (t >= 0)
 		return (t);
-	else 
+	else
 		return (-t);
 }
