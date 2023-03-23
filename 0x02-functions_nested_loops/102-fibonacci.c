e33961f22c12d885beba75f6bc6a275fa5da525e
lujaja <jarzcyber@gmail.com>
2023-03-22 17:33:52 -0700

    ALX S.E c programming practical

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,14 +14,14 @@ int main(void)
 	while (i < 50)
 	{
 		if (i == 0)
-			printf("%d ", num1);
+			printf("%ld ", num1);
 		else if (i == 1)
-			printf(", %d", num2);
+			printf(", %ld", num2);
 		else
 		{
 			num2 += num1;
 			num1 = num2 - num1;
-			printf(", %d", num2;
+			printf(", %ld", num2;
 		}
 		++i;
 	}
