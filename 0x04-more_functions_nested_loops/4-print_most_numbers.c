e76e37273b1aa465013e31841ba1c7f4e90555b4
lujaja <jarzcyber@gmail.com>
2023-03-24 03:31:10 -0700

    ALX S.E c programming practical

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_most_numbers - print from 0 to 9 skipping 2 and 4.
+  */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || 4)
+			continue
+		putchar(i + '0');
+	}
+	putchar('\n');
+}
