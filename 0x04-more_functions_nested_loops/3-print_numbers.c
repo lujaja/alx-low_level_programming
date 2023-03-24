a22e04f9fbe44387d6d9e0bb1d39d36234d95c3c
lujaja <jarzcyber@gmail.com>
2023-03-24 02:23:35 -0700

    ALX S.E c programming practical

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * print_numbers - prints 0 to 9
+  *
+  * Return: nothing!
+  */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+		_putchar(i);
+	_putchar('\n');
+}
