f1e02deffb8c82de18d6ca6b54fc167a71d6ccb5
lujaja <jarzcyber@gmail.com>
2023-03-24 02:10:34 -0700

    ALX S.E c programming practical

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ *
+ * @a: character
+ * @b: character
+ * Return: results
+ */
+
+int mul(int a, int b)
+{
+	int results;
+
+	results = a * b;
+
+	return (results);
+}
