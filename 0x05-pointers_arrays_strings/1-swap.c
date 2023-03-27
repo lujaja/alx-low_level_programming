b0a4afdff806d80cd5f2b4c303701078148152d6
lujaja <jarzcyber@gmail.com>
2023-03-27 02:22:21 -0700

    ALX S.E c programming practical

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of two integers
+ * @a: pointer variable
+ * @b: pointer variable
+ * lujaja
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
