d4e4d397d9151c311490c49694cc761d95f99e51
lujaja <jarzcyber@gmail.com>
2023-03-27 01:57:28 -0700

    ALX S.E c programming practical

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of variable n to 98.
+ * @n: variable
+ * lujaja
+ */
+
+void reset_to_98(int *n)
+{
+	int ptr_n = &n;
+
+	*ptr_n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+I am lujaja, currently S.E Students @ALX.
+0x05-pointers_arrays_strings practicals
