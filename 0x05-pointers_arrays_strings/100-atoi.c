927d8588076589aac6b4e08c9050e8f1a284eb0a
lujaja <jarzcyber@gmail.com>
2023-03-27 13:15:18 -0700

    ALX S.E c programming practical

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+
+#include "main.h"
+
+
+/**
+ * _atoi - converts string to inegers
+ * @s: string pointer
+ * Return: 0 is string cant be converted 
+ */
+
+int _atoi(char *s)
+{
+	register int i = 0, results = 0, sign;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] < 48 || s[i] > 57)
+		return (0);
+		else if (s[i] == 45)
+			sign += 1;
+		else
+		{
+			results = results * 10 + (s[i] - 48);
+			if (sign % 2 != 0)
+				results = -results;results = -results;
+		}
+	}
+}
+
