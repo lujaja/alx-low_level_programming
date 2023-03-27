507197dc8792a10442ba0eaec215b636267a4a23
lujaja <jarzcyber@gmail.com>
2023-03-27 02:45:16 -0700

    ALX S.E c programming practical

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+
+#include "main.h"
+
+/**
+ * _strlen - lenth of  string
+ * @s: pointer
+ * lujaja
+ * Return: total number of strings
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i == 1; s[i]; i++)
+		return (i);
+}
