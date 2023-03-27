1e8f9b161f1c7d2a012b016deacbecae34f318d8
lujaja <jarzcyber@gmail.com>
2023-03-27 07:55:26 -0700

    ALX S.E c programming practical

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,6 @@
 
 
+
 #include "main.h"
 
 /**
@@ -15,9 +16,9 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		if (i == 0)
-			printf("%d", *a[i]);
+			printf("%d", a[i]);
 		else
-			printf(", %d", *a[i]);
-		putchar("\n");
+			printf(", %d", a[i]);
+		printf("\n");
 	}
 }
