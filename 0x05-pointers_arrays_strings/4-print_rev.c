33f5682f29a6cc6342b0c355407144760fe8395b
lujaja <jarzcyber@gmail.com>
2023-03-27 03:38:20 -0700

    ALX S.E c programming practical

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+
+#include "main.h"
+
+/**
+ * print_rev - revers a string
+ * @s: string pointer
+ * lujaja
+ */
+
+void print_rev(char *s)
+{
+	register int i, j, len;
+
+	i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
+		putchar(s[j];
+	putchar('\n');
+}
