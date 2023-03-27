c925555ecfdf00d806344c7f87a7710e549d24a9
lujaja <jarzcyber@gmail.com>
2023-03-27 07:04:16 -0700

    ALX S.E c programming practical

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+
+#include "main.h"
+
+/**
+ * puts_half(char *str)
+ * @str: string pointer
+ */
+
+void puts_half(char *str)
+{
+	register int n, i, len = 0;
+
+	while (str[len] != '\0')
+		len++
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+			putchar(str[i]);
+	}
+	else
+	{
+		for (n = (len - 1) /2; n < len - 1; n++)
+			putchar(str[n + 1]);
+	}
+	putchar('\n');
+}
