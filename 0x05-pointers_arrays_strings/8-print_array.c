3a167d262ec8f80f373219eff2fb0dfb2f9436b0
lujaja <jarzcyber@gmail.com>
2023-03-27 07:48:50 -0700

    ALX S.E c programming practical

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+
+
+#include "main.h"
+
+/**
+ * print_array - prints array of numbers
+ * @a: pointer to an array
+ * @n: variable
+ */
+
+void print_array(int *a, int n)
+{
+	register int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+		putchar("\n");
+	}
+}
