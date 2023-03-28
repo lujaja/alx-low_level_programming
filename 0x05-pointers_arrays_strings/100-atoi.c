f646d1cd34ed132cacb14bc5c62e6cc401ee73da
lujaja <jarzcyber@gmail.com>
2023-03-28 05:49:13 -0700

    ALX S.E c programming practical

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,5 @@
 
 
-
 #include "main.h"
 
 
@@ -13,21 +12,30 @@
 int _atoi(char *s)
 {
 	register int i, len;
-	int num, terminator;
+	int digit, sign, num, terminator;
 
-	len = 0;
-	i = 0;
+	i = sign = num = len = terminator = digit = 0;
 
 	while (s[len] != '\0')
 		len++;
-
-	while (i < len)
+	while (i < len && terminator == 0)
 	{
-		if (s[i] < 48 || s[i] > 58)
+		if (s[i] == '-')
+			++sign;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (sign % 2 != 0)
+				digit = -digit;
+			num = num * 10 + digit;
+			terminator = 1;
+			if (s[i] < '0' || s[i] > '9')
+				break;
 			terminator = 0;
-		else if
-			num = s[i] - 48;
-		else
-			return (terminator);
+		}
+		i++;
 	}
+	if (terminator == 0)
+		return (0);
+	return (num);
 }
