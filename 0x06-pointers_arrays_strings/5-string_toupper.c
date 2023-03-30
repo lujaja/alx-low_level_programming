a06a2e90421fc5efbddfbfd946c9c5fccdb48989
lujaja <jarzcyber@gmail.com>
2023-03-30 02:01:13 -0700

    ALX S.E c programming practical

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+
+#include "main.h"
+/**
+ * *string_toupper - function that compares two strings
+ * @string: paramater for array
+ * Return: Always 0.
+ */
+char *string_toupper(char *string)
+{
+	int upper = 'a' - 'A';
+
+	while (*string)
+	{
+		if (*string >= 'a' && *string <= 'z')
+			*string -= upper;
+		*string++;
+	}
+	return (string);
+}
