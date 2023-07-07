ddecc79be6e42643b2c8048302ca006ad51e63d7
lujaja <jarzcyber@gmail.com>
2023-07-07 12:08:36 +0300

    correction

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,25 +1,27 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * free_listint2 - Frees a listint_t list.
- * @head: A pointer to the address of the
- *        head of the listint_t list.
+ * free_listint2 - function that frees a singly linked list.
+ * @head: pointer to pointer to the head of linked list.
  *
- * Description: Sets the head to NULL.
+ * Make sure there is no memory leaks.
+ * The function sets the head to NULL.
+ *
+ * Return: No return.
  */
 
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
+	listint_t *temp_variable;
 
 	if (head == NULL)
-		return;
-
-	while (*head)
+		return; /* do not do anything */
+	while (*head != NULL)
 	{
-		tmp = *head;
+		temp_variable =  *head;
 		*head = (*head)->next;
-		free(tmp);
+		free(temp_variable);
 	}
-	head = NULL;
+	/* *head = NULL; */ /* not needed */
 }
--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,10 +1,12 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * sum_listint - returns the sum of all the data (n)
- * @head: pointer to the head of a list
- * Return: sum of all the data (n)
- *         Otherwise - 0 if the list is empty
+ * sum_listint - function that returns the sum of all the data (n) of a,
+ * listint_t linked list.
+ * @head: pointer to the head of linked list.
+ *
+ * Return:  sum of all the data (n) of a linked list or 0 if list is empty.
  */
 
 int sum_listint(listint_t *head)
@@ -16,6 +18,5 @@ int sum_listint(listint_t *head)
 		sum += head->n;
 		head = head->next;
 	}
-
 	return (sum);
 }
