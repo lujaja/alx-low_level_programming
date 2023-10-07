53f3af2a21255cc3da2fac4481d6c1ffa1de2a87
lujaja <jarzcyber@gmail.com>
2023-10-07 10:19:31 +0300

    hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,9 +1,8 @@
-/****************
-* Lujaja Luvuga *
-*****************/
-
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
+/*****************
+ * Lujaja Luvuga *
+ * ***************/
 
 /**************************
  * Standard C headerfiles *
@@ -31,9 +30,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -46,8 +45,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 /***********************
@@ -57,7 +56,7 @@ hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-hash_node_t *create_new_node(const char *key,const char * value);
+hash_node_t *create_new_node(const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 
